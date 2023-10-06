46a660da5161ad7265f8a794882043fbfac0969c
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-10-06 19:32:30 +0200

    added getters and setters to Libro

--- unidad9-3/src/Libro.java
+++ unidad9-3/src/Libro.java
@@ -18,5 +18,43 @@ public class Libro{
 		this.titulo = tit;
 		this.autor = autor;
 	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public String toString() {
+		return " El Libro"+titulo+" con ISBN:" + isbn + " creado por el autor "+autor+" tiene paginas=" + paginas + ".";
+	}
+	
 	
 }
